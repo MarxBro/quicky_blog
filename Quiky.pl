207af000af89fc57ca5577ab8dec7935fdb32d95
MarxBro <allthemarxbrothers@gmail.com>
2016-03-14 18:24:11 -0300

    css del orto

--- Quiky.pl
+++ Quiky.pl
@@ -382,7 +382,7 @@ Hola este un blog personal... o algo así.
 
 Me acostumbré a firmar como __MarxBro__ y eso no creo que cambie en un futuro cercano.
 
-Acá escribo lo que me va pareciendo relevante o simplemente, lo que tengo ganas de escribir.
+Acá escribo lo que me va pareciendo relevante o simplemente, lo que tengo ganas de escribir. 
 No es gran cosa y responde casi exclusivamente a mis caprichos informáticos (sobre Perl, Linux y lo que vaya apareciendo).
 
 Mi perfil en github y mis repositorios están [a su disposición](https://github.com/MarxBro).
